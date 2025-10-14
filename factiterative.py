b3b51a13dc98236ac580ce6eac22983febcd4c87
ideallydead <108585714+ideallydead@users.noreply.github.com>
2025-10-14 19:20:13 +0530

    factiterative.py

---
+++ factiterative.py
@@ -0,0 +1,20 @@
+def inputnum():
+    x=int(input())
+    if x<0 or x>100:
+        print("Wrong input")
+    return x
+
+def zero(x):
+    if x==0:
+        print(1)
+
+def fact(x):
+    for a in range(1,x+1):
+        f=1
+        for b in range(1,a+1):
+            f=f*b
+        print(f)
+
+x=inputnum()
+zero(x)
+fact(x)
